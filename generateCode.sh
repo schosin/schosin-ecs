5e38991595a93a3d1cf16c08a00a0adabcdfac8b
Ken Schosinsky <schosins@gmail.com>
2025-05-30 17:08:32 +0200

    Remove generateCode.sh
    Used while developing code generation, not needed anymore

--- generateCode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn install -pl codegen && rm -rf api/target/classes/de/schosin/ecs/api/Pooled.class && rm -rf engine/target/classes && mvn compile -pl api,core,plugin/archetype,plugin/transmuter,plugins/composition -e
-
